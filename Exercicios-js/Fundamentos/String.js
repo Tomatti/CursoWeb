33c351b5cd4990294bd5d145873780fddc067ce4
Tomatti <74329034+Tomatti@users.noreply.github.com>
2020-11-12 21:55:12 -0300

    dia 1 segunda árte

---
+++ Exercicios-js/Fundamentos/String.js
@@ -0,0 +1,10 @@
+const escola = "cod3r"
+console.log (escola.charAt(4))
+console.log (escola.charAt(5))
+
+console.log (escola.charCodeAt(4))
+console.log (escola.indexOf(4))
+console.log (escola.substr(0, 2))
+console.log ('Escola '.concat(escola).concat("!"))
+console.log (escola.replace( 3, 'e'))
+console.log ('Mria, Ana, pedro'.split(','))
